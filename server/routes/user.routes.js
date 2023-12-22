2a4c62949d9d46baccbdb2c2ee7e7a0f9a54561c
sumit <mazumdarsumit3@gmail.com>
2023-12-22 22:01:41 +0530

    defined user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,4 +3,8 @@ import { Router } from 'express';
 const router = Router();
 
 router.post('/register', register);
+router.post('/login', login);
+router.get('/logout', logout);
+router.get('/me', getProfile);
+
 export default router;
