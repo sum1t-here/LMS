4b7e92b2a29b5b22037f5cf53163f8f30947f13c
sumit <mazumdarsumit3@gmail.com>
2023-12-22 21:20:05 +0530

    Connected to database

---
+++ server/config/dbConnection.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', false);
+
+const connectionToDb = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
+
+    if (connection) {
+      console.log(`Connected to MongoDB: ${connection.host}`);
+    }
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+};
+
+export default connectionToDb;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import app from './app.js';
+import connectionToDb from './config/dbConnection.js';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
+  await connectionToDb();
   console.log(`App is running at http://localhost:${PORT}`);
 });
